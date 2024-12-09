7f157174ddae994c487f3016828dc610a9e0e8ab
SHAHZAIbSAHITO <sahitoshahzaib888@gmail.com>
2024-12-09 21:46:22 +0500

    first commit

--- src/app/Product/[id]/page.tsx
+++ src/app/Product/[id]/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import React from 'react'
-import { useParams } from 'next/navigation'; // Hook to get params from the URL
+import { useParams } from 'next/navigation'; 
 import ProductDetail from '@/components/revews/details';
 
 const Daynapicproductsd = () => {
-  // Fetch dynamic params using useParams
+
   const { id } = useParams();
   
-  // Convert the string id to a number
+  
   const productId = Number(id);
 
   return (
